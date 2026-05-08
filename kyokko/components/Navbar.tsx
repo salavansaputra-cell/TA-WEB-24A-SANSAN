5f623eb385acaeb71d8a631072c4a4f39ae67855
aurly1717 <aurly_zacci_wardani@teknokrat.ac.id>
2026-05-08 17:55:20 +0700

    Aurly : menambahkan menu desktop navbar

--- kyokko/components/Navbar.tsx
+++ kyokko/components/Navbar.tsx
@@ -1,16 +1,27 @@
+import Link from "next/link";
+
 export default function Navbar() {
   return (
     <nav className="fixed top-0 left-0 w-full z-50 bg-black/40 backdrop-blur-md text-white shadow-md">
-
       <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
-
         {/* LOGO */}
-        <h1 className="text-2xl font-bold tracking-wide">
-          Kyokko Beach 🌊
-        </h1>
+        <h1 className="text-2xl font-bold tracking-wide">Kyokko Beach 🌊</h1>
 
+        {/* DESKTOP MENU */}
+        <ul className="hidden md:flex gap-8 font-medium items-center">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+
+          <li>
+            <Link href="/facilities">Facilities</Link>
+          </li>
+
+          <li>
+            <Link href="/info-wisata">Info Wisata</Link>
+          </li>
+        </ul>
       </div>
-
     </nav>
   );
 }
