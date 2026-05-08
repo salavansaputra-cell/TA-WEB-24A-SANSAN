08d0addc105deb7581e8123c7d488e195522f393
salavanalifsaputra <salavan_alif_saputra@teknokrat.ac.id>
2026-05-08 18:19:33 +0700

    Salavan:menambahkan spasing halaman wisata

--- kyokko/app/info-wisata/page.tsx
+++ kyokko/app/info-wisata/page.tsx
@@ -1,6 +1,6 @@
 export default function InfoWisataPage() {
   return (
-     <div className="min-h-screen bg-gray-100">
+     <div className="min-h-screen bg-gray-100 pt-28 py-16 px-6">
     </div>
   );
 }
