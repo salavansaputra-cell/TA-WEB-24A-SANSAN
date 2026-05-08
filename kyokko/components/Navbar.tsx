fa75a250a1bfd3fec2853bcaf52dfbb300535c6a
aurly1717 <aurly_zacci_wardani@teknokrat.ac.id>
2026-05-08 17:57:09 +0700

    Aurly : menambahkan efek hover pada menu navbar

--- kyokko/components/Navbar.tsx
+++ kyokko/components/Navbar.tsx
@@ -10,23 +10,37 @@ export default function Navbar() {
         {/* DESKTOP MENU */}
         <ul className="hidden md:flex gap-8 font-medium items-center">
           <li>
-            <Link href="/">Home</Link>
+            <Link href="/" className="hover:text-blue-300 transition">
+              Home
+            </Link>
           </li>
 
           <li>
-            <Link href="/facilities">Facilities</Link>
+            <Link href="/facilities" className="hover:text-blue-300 transition">
+              Facilities
+            </Link>
           </li>
 
           <li>
-            <Link href="/info-wisata">Info Wisata</Link>
+            <Link
+              href="/info-wisata"
+              className="hover:text-blue-300 transition"
+            >
+              Info Wisata
+            </Link>
           </li>
 
           <li>
-            <Link href="/reservasi">Reservasi</Link>
+            <Link href="/reservasi" className="hover:text-blue-300 transition">
+              Reservasi
+            </Link>
           </li>
 
           <li>
-            <Link href="/login" className="bg-blue-500 px-4 py-2 rounded-lg">
+            <Link
+              href="/login"
+              className="bg-blue-500 px-4 py-2 rounded-lg hover:bg-blue-600 transition"
+            >
               Login
             </Link>
           </li>
