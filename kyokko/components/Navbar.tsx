0df014ce98c590078eef5dd3ef8d8cba2d8ef0f1
aurly1717 <aurly_zacci_wardani@teknokrat.ac.id>
2026-05-08 17:53:45 +0700

    Aurly : menambahkan styling dasar navbar

---
+++ kyokko/components/Navbar.tsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+  return (
+    <nav className="fixed top-0 left-0 w-full z-50 bg-black/40 backdrop-blur-md text-white shadow-md">
+
+      <div className="max-w-7xl mx-auto px-6 py-4">
+        Navbar
+      </div>
+
+    </nav>
+  );
+}
