0e2588e0c8a7be20faf01bb9353f0e8b0903fe59
aurly1717 <aurly_zacci_wardani@teknokrat.ac.id>
2026-05-08 18:03:54 +0700

    Aurly : menambahkan container dan grid footer

--- kyokko/components/Footer.tsx
+++ kyokko/components/Footer.tsx
@@ -1,7 +1,11 @@
 export default function Footer() {
   return (
     <footer className="bg-black text-white py-10 px-6 mt-10">
-      Footer
+
+      <div className="max-w-6xl mx-auto grid md:grid-cols-3 gap-8">
+        Footer
+      </div>
+
     </footer>
   );
 }
