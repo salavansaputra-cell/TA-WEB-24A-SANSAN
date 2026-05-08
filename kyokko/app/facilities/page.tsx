3c086a1f4d262dfcadf2efe4b36b529d06003df1
nadiarizky-star <nadia_risky_juniarti@teknokrat.ac.id>
2026-05-08 22:10:37 +0700

    nadia : mempercantik title halaman

--- kyokko/app/facilities/page.tsx
+++ kyokko/app/facilities/page.tsx
@@ -1,8 +1,10 @@
 export default function FacilitiesPage() {
   return (
     <div className="min-h-screen bg-gray-100 pt-28 py-16 px-6">
-      <div className="text-center">
-        <h1>Facilities Kyokko Beach</h1>
+      <div className="text-center mb-14">
+        <h1 className="text-4xl font-bold">
+          Facilities Kyokko Beach 🌊
+        </h1>
       </div>
     </div>
   );
