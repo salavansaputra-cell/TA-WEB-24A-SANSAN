3661f32db48f491dc6e491d42b26b2efe72f34ff
aurly1717 <aurly_zacci_wardani@teknokrat.ac.id>
2026-05-08 18:04:41 +0700

    Aurly : menambahkan section brand footer

--- kyokko/components/Footer.tsx
+++ kyokko/components/Footer.tsx
@@ -3,7 +3,20 @@ export default function Footer() {
     <footer className="bg-black text-white py-10 px-6 mt-10">
 
       <div className="max-w-6xl mx-auto grid md:grid-cols-3 gap-8">
-        Footer
+
+        {/* BRAND */}
+        <div>
+
+          <h2 className="text-2xl font-bold mb-3">
+            Kyokko Beach 🌊
+          </h2>
+
+          <p className="text-gray-400">
+            Destinasi wisata modern di Lampung dengan konsep beach club.
+          </p>
+
+        </div>
+
       </div>
 
     </footer>
