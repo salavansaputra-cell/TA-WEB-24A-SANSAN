80e12b779d9b568b6fc62f596cc395aa9d18a193
nadiarizky-star <nadia_risky_juniarti@teknokrat.ac.id>
2026-05-08 22:09:02 +0700

    nadia : menambahkan spacing halaman

--- kyokko/app/facilities/page.tsx
+++ kyokko/app/facilities/page.tsx
@@ -1,6 +1,6 @@
 export default function FacilitiesPage() {
   return (
-    <div className="min-h-screen bg-gray-100">
+    <div className="min-h-screen bg-gray-100 pt-28 py-16 px-6">
       <h1>Facilities Kyokko Beach</h1>
     </div>
   );
