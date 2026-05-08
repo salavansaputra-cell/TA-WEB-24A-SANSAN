0f1a104642ab80f299c1b136bc70684778e1567d
aurly1717 <aurly_zacci_wardani@teknokrat.ac.id>
2026-05-08 17:54:31 +0700

    Aurly : menambahkan logo navbar

--- kyokko/components/Navbar.tsx
+++ kyokko/components/Navbar.tsx
@@ -2,8 +2,13 @@ export default function Navbar() {
   return (
     <nav className="fixed top-0 left-0 w-full z-50 bg-black/40 backdrop-blur-md text-white shadow-md">
 
-      <div className="max-w-7xl mx-auto px-6 py-4">
-        Navbar
+      <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
+
+        {/* LOGO */}
+        <h1 className="text-2xl font-bold tracking-wide">
+          Kyokko Beach 🌊
+        </h1>
+
       </div>
 
     </nav>
