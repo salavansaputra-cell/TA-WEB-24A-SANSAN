97e12a61d492889e86fccf0cb5a937590bbe0931
nadiarizky-star <nadia_risky_juniarti@teknokrat.ac.id>
2026-05-08 22:17:06 +0700

    nadia : membuat grid layout fasilitas

--- kyokko/app/facilities/page.tsx
+++ kyokko/app/facilities/page.tsx
@@ -55,7 +55,7 @@ export default function FacilitiesPage() {
         </p>
       </div>
 
-      <div className="max-w-6xl mx-auto">
+      <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-8 max-w-6xl mx-auto">
       </div>
     </div>
   );
