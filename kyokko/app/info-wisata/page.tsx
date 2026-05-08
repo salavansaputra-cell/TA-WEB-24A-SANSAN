30905925612b0574ba3b7a43ca03e152e4c32966
salavanalifsaputra <salavan_alif_saputra@teknokrat.ac.id>
2026-05-08 18:22:49 +0700

    Salavan:menambahkan deskripsi halaman wisata

--- kyokko/app/info-wisata/page.tsx
+++ kyokko/app/info-wisata/page.tsx
@@ -3,6 +3,10 @@ export default function InfoWisataPage() {
      <div className="min-h-screen bg-gray-100 pt-28 py-16 px-6">
         <div className="text-center mb-14">
   <h1>Info Wisata Kyokko Beach 🌊</h1>
+  <p className="text-gray-600 max-w-3xl mx-auto">
+  Nikmati berbagai aktivitas wisata dan pilihan penginapan terbaik di
+  Kyokko Beach.
+</p>
 </div>
     </div>
   );
