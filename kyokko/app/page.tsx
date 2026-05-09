98179e85add24ec5d108fe60b2630e6924b59b04
aurly1717 <aurly_zacci_wardani@teknokrat.ac.id>
2026-05-09 16:17:05 +0700

    aurly: menambahkan hero section homepage

--- kyokko/app/page.tsx
+++ kyokko/app/page.tsx
@@ -1,7 +1,14 @@
 export default function Home() {
   return (
     <main className="bg-gray-100">
-      Home Page
+      {/* HERO SECTION */}
+      <section
+        className="h-screen bg-cover bg-center flex items-center justify-center px-6 relative"
+        style={{
+          backgroundImage:
+            "url('https://images.unsplash.com/photo-1507525428034-b723cf961d3e')",
+        }}
+      ></section>
     </main>
   );
 }
