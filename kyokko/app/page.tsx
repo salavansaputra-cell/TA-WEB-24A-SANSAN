10255c652f95bc051a6483f387119b7ab0f358e8
aurly1717 <aurly_zacci_wardani@teknokrat.ac.id>
2026-05-09 16:18:25 +0700

    aurly: menambahkan konten hero section

--- kyokko/app/page.tsx
+++ kyokko/app/page.tsx
@@ -9,8 +9,18 @@ export default function Home() {
             "url('https://images.unsplash.com/photo-1507525428034-b723cf961d3e')",
         }}
       >
-        {/* OVERLAY */}
         <div className="absolute inset-0 bg-black/30"></div>
+
+        {/* CONTENT */}
+        <div className="relative z-10 text-center text-white max-w-2xl">
+          <p className="uppercase tracking-[6px] text-sm text-blue-200 mb-4">
+            Welcome to
+          </p>
+
+          <h1 className="text-5xl md:text-6xl font-bold mb-6">
+            Kyokko Beach 🌊
+          </h1>
+        </div>
       </section>
     </main>
   );
