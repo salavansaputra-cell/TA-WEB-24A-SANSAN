2d728d3bdd400dac2bc573a9c012ff70e90aef38
salavanalifsaputra <salavan_alif_saputra@teknokrat.ac.id>
2026-06-08 17:18:56 +0700

    salavan: menampilkan foto standar-room di info wisata

--- kyokko/app/info-wisata/page.tsx
+++ kyokko/app/info-wisata/page.tsx
@@ -50,7 +50,7 @@ const penginapan = [
   {
     title: "Standard Room",
     price: 350000,
-    image: "https://images.unsplash.com/photo-1566073771259-6a8506099945",
+    image: "/images/standar-room.jpeg",
 
     desc: "Kamar nyaman untuk pasangan atau keluarga kecil.",
   },
