1fd5b10a3d26f4386cf201c2334e7b763368f85d
salavanalifsaputra <salavan_alif_saputra@teknokrat.ac.id>
2026-06-13 15:11:18 +0700

    salavan:menambahkan layout halaman pesanan

--- app/pesanan/page.tsx
+++ app/pesanan/page.tsx
@@ -46,3 +46,13 @@ export default function PesananPage() {
       </main>
     );
   }
+
+  return (
+  <main className="min-h-screen bg-gray-50 pt-28 pb-20 px-6">
+    <div className="max-w-5xl mx-auto">
+      <h1>Pesanan Saya</h1>
+    </div>
+  </main>
+);
+
+}
