2eb5dce10ca4a8e319ae633dae28c0336d1f331c
salavanalifsaputra <salavan_alif_saputra@teknokrat.ac.id>
2026-06-13 14:54:44 +0700

    salavan: menambahkan state data reservasi

--- app/pesanan/page.tsx
+++ app/pesanan/page.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useState } from "react";
+
 type Reservasi = {
   nama: string;
   tanggal: string;
@@ -7,6 +11,8 @@ type Reservasi = {
 };
 
 export default function AdminPage() {
+  const [data, setData] = useState<Reservasi[]>([]);
+
   return (
     <div>
       Dashboard Admin
