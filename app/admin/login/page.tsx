33763b0c867486b0ceae6820c0073be4fd86b492
nadiarizky-star <nadia_rizky_juniarti@teknokrat.ac.id>
2026-06-13 15:44:21 +0700

    Nadia: menambahkan state dan router pada halaman login

---
+++ app/admin/login/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+export default function AdminLoginPage() {
+  const router = useRouter();
+
+  return (
+    <main>
+      Admin Login
+    </main>
+  );
+}
